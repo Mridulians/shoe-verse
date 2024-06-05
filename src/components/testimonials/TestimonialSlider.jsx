28e65fd8eba97c7a95a2c8c0f41319a2f528a2c6
Mridulians <gmridul898@gmail.com>
2024-06-05 17:53:57 +0530

    new changes

--- src/components/testimonials/TestimonialSlider.jsx
+++ src/components/testimonials/TestimonialSlider.jsx
@@ -1,7 +1,32 @@
-// import React from 'react'
+/* eslint-disable react/prop-types */
+// import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import StarTest from "../../assets/StarTest.png";
-import Carousel from "react-elastic-carousel";
 import './Test.css'
+
+const CustomPrevArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", zIndex: 2 }}
+      onClick={onClick}
+    />
+  );
+};
+
+const CustomNextArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", zIndex: 2 }}
+      onClick={onClick}
+    />
+  );
+};
 
 const TestimonialSlider = () => {
   const data = [
@@ -62,20 +87,53 @@ const TestimonialSlider = () => {
       id: 7,
     },
   ];
-  const breakPoints = [
-    { width: 1, itemsToShow: 1 },
-    { width: 550, itemsToShow: 2, itemsToScroll: 2, pagination: false },
-    { width: 850, itemsToShow: 3 },
-    { width: 1150, itemsToShow: 4, itemsToScroll: 2 },
-    { width: 1450, itemsToShow: 5 },
-    { width: 1750, itemsToShow: 6 },
-  ]
+
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    arrows: true,
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
   return (
     <div className="w-[100%] md:w-[70%] m-auto pb-[10rem] relative">
-      <Carousel itemsToShow={3} enableAutoPlay autoPlaySpeed={1500} breakPoints={breakPoints}>
+      <Slider {...settings}>
         {data.map((item) => (
-          <div key={item.id} className="bg-gray-50 shadow-inner shadow-slate-500 mx-[2rem] p-[1rem] h-[300px]">
+          <div
+            key={item.id}
+            className="bg-gray-50 shadow-inner shadow-slate-500 mx-[2rem] p-[1rem] h-[300px]"
+          >
             <div>
               <img src={item.img} alt="" className="w-[4rem]" />
               <p className="font-bold font-sans">{item.title}</p>
@@ -88,7 +146,7 @@ const TestimonialSlider = () => {
             </div>
           </div>
         ))}
-      </Carousel>
+      </Slider>
     </div>
   );
 };
